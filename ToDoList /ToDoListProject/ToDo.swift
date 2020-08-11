332b9cf6600d3d97f72a2d798ec0fb957ba74b97
suah kim <69254579+suahkim@users.noreply.github.com>
2020-08-11 23:23:28 +0900

    Iteration 0

---
+++ ToDoList /ToDoListProject/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  ToDoListProject
+//
+//  Created by Suah Kim on 2020/08/11.
+//  Copyright © 2020 Su Ah Kim. All rights reserved.
+//
+
+import UIKit
+
+class ToDoClass {
+    
+    var description = ""
+    var important = false
+    
+}
